2109ec6c73e7398bd2aa02ced735d1cf411adade
Mike Gore <mikegore1000@msn.com>
2016-10-03 20:28:36 +0100

    Added TopShelf to provide Windows Service support

--- ApplicationInsights.ServerAgent/ApplicationInsights.ServerAgent/Program.cs
+++ ApplicationInsights.ServerAgent/ApplicationInsights.ServerAgent/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights.Extensibility;
+using Topshelf;
 
 namespace ApplicationInsights.ServerAgent
 {
@@ -12,9 +13,35 @@ namespace ApplicationInsights.ServerAgent
     {
         static void Main(string[] args)
         {
-            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
+            HostFactory.Run(x =>
+            {
+                x.Service<ServerAgent>(s =>
+                {
+                    s.ConstructUsing(() => new ServerAgent());
+                    s.WhenStarted(sa => sa.Start());
+                    s.WhenStopped(sa => sa.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Appliation Insights Server Agent");
+                x.SetDisplayName("Appliation Insights Server Agent");
+                x.SetServiceName("ApplicationInsights.ServerAgent");
+            });
 
             Console.ReadLine();
+        }
+
+        public class ServerAgent
+        {
+            public void Start()
+            {
+                TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
+            }
+
+            public void Stop()
+            {
+                
+            }
         }
     }
 }
