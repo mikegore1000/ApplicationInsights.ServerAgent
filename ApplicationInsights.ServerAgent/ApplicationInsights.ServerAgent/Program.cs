1726354db27a491da0b375c6402000dc82b3cf65
Mike Gore <mikegore1000@msn.com>
2016-10-03 20:18:52 +0100

    Initial commit

---
+++ ApplicationInsights.ServerAgent/ApplicationInsights.ServerAgent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace ApplicationInsights.ServerAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["InstrumentationKey"];
+
+            Console.ReadLine();
+        }
+    }
+}
